31994842462472f579542625737086cc242eb7ad
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-09-29 18:13:05 -0300

    Refactor user schema by removing unnecessary whitespace for improved readability

--- src/model/mongo/user.mjs
+++ src/model/mongo/user.mjs
@@ -1,17 +1,14 @@
-import mongoose from "mongoose";
-
 const userSchema = new mongoose.Schema({
-
     name: { type: String, required: true },
-
     password: { type: String, required: true },
-
     email: { type: String, required: true },
-
-    role: { type: String, required: true, default: "user" }
-},
-{
-
+    role: { 
+        type: String, 
+        enum: ["user", "admin", "moderator"], 
+        required: true, 
+        default: "user" 
+    }
+}, {
     timestamps: true
 });
 export default mongoose.model("User", userSchema);
