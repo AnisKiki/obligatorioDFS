2fd679bdfffe2eaf30f8a439e8d30cbb14bd8485
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-09-30 17:25:09 -0300

    Add capacity and unlimited fields to plan schema

--- src/model/schemas/planSchema.mjs
+++ src/model/schemas/planSchema.mjs
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const planSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
+    capacity: { type: Number, required: true },
+    unlimited: { type: Boolean, default: false }
 }, {
     timestamps: true
 });
