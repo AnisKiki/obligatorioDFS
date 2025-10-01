263b7b9f9b12fda2464ca49e098820e9faf8cf4e
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-10-01 19:06:55 -0300

    Comment out Plan model and planSchema imports and definitions

--- src/model/schemas/planSchema.mjs
+++ src/model/schemas/planSchema.mjs
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const planSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    capacity: { type: Number, required: true },
-    unlimited: { type: Boolean, default: false }
-}, {
-    timestamps: true
-});
-export default planSchema;
+// const planSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     price: { type: Number, required: true },
+//     capacity: { type: Number, required: true },
+//     unlimited: { type: Boolean, default: false }
+// }, {
+//     timestamps: true
+// });
+// export default planSchema;
