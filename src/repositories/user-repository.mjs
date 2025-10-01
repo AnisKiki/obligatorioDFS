32aa586b2d6eb8bd808b360ee7e9a7a6c738533e
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-10-01 19:50:33 -0300

    Simplify user repository initialization by removing unnecessary conditional checks

--- src/repositories/user-repository.mjs
+++ src/repositories/user-repository.mjs
@@ -3,17 +3,10 @@ import userMongoRepository from "./mongo-repository/user-mon-Repository.mjs";
 import { baseConstant } from "../constants/base-constant.mjs";
 import 'dotenv/config';
 
-// ver comentarios en user-repository
+
 let userRepository;
-/* if (process.env.DB === "mysql") {
-  userRepository = userMongoRepository;
-} else {
-  userRepository = userMongoRepository;
-}*/
-if (process.env.BASE_IN_USE == baseConstant.MONGO) {
-  userRepository = userMongoRepository;
-}else{
-  userRepository = userMongoRepository;
-}
+
+userRepository = userMongoRepository;
+
 
 export default userRepository; 
