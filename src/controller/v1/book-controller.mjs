1a8642abc01c56eb7efce3eaee085b7802db9baa
AnisKiki <nadyrmiguel77@gmail.com>
2025-10-02 20:08:59 -0300

    Implement createBook function for book creation with validation

---
+++ src/controller/v1/book-controller.mjs
@@ -0,0 +1,29 @@
+import bookRepository from "../../repositories/book-repository.mjs";
+import "dotenv/config";
+
+export async function createBook(req, res) {
+    try {
+        const { tittle, authors, publishedDate, description, categories } = req.body;
+
+        const existing = await bookRepository.getbookByTitle(title); //Verificar si ya existe el titulo
+        if (existing) return res.status(400).json({ message: "Libro ya existente" });
+
+        const book = await bookRepository.create({  //Crear libro
+            tittle, 
+            authors, 
+            publishedDate, 
+            description, 
+            categories
+        });
+        res.status(201).json({
+            id: book._id, 
+            title: book.title, 
+            authors: book.authors, 
+            publishedDate: book.publishedDate, 
+            description: book.description, 
+            categories: book.categories
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
