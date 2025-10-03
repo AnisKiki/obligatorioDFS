63cd233b1272e5810992ad0cdff7a9614b2a7ca0
AnisKiki <nadyrmiguel77@gmail.com>
2025-10-02 23:41:32 -0300

    Add validation schema for creating and deleting books

--- src/validations/validation-book.mjs
+++ src/validations/validation-book.mjs
@@ -1,7 +1,12 @@
 import Joi from "joi"
 
 export const validateCreate = Joi.object({
-    name: Joi.string().min(3).max(40).required(),
-    email: Joi.string().regex(/.+@.+\..+/).required(),
-    password: Joi.string().min(3).max(20).required(),
+    titlee: Joi.string().min(3).max(40).required(),
+    authors: Joi.string().min(3).max(40).required(),
+    publishedDate: Joi.date().min("1800-01-01").max("now").required(),
+    description: Joi.string().min(3).max(40).required(),
+    categories: Joi.string().min(3).max(40).required()
+});
+export const validateDelete = Joi.object({
+    id: Joi.string().hex().length(24).required()
 });
