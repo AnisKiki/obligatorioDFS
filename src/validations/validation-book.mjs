09e36977da78c8a7017ec6bf9dd10f887fe76c71
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-10-06 19:06:15 -0300

    Add validation for update body and parameters in book operations

--- src/validations/validation-book.mjs
+++ src/validations/validation-book.mjs
@@ -37,3 +37,24 @@ export const validateDelete = Joi.object({
     "string.pattern.base": "El campo {#label} debe ser un email válido",
     "any.required": "El campo {#label} es obligatorio",
 });
+
+export const validateUpdateParams = Joi.object({
+    id: Joi.string().hex().length(24).required()
+}).messages({
+    "string.empty": "El campo {#label} es obligatorio",
+    "any.required": "El campo {#label} es obligatorio",
+});
+
+export const validateUpdateBody = Joi.object({
+    title: Joi.string().min(3).max(40).required(),
+    authors: Joi.string().min(3).max(40).required(),
+    publishedDate: Joi.date().min(MinFechLibro).max("now").required(),
+    description: Joi.string().min(3).max(1000).required(),
+    categories: Joi.string().min(3).max(40).required()
+}).messages({
+    "string.empty": "El campo {#label} es obligatorio",
+    "any.required": "El campo {#label} es obligatorio",
+    "date.base": "El campo {#label} debe ser una fecha válida",
+    "date.min": "El campo {#label} debe ser posterior a {#limit}",
+    "date.max": "El campo {#label} debe ser anterior o igual a la fecha actual"
+});
