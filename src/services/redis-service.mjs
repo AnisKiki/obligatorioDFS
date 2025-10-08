824bb99d1ecbaaad8286475624a8aa902992b64d
AnisKiki <nadyrmiguel77@gmail.com>
2025-10-07 23:56:42 -0300

    Add comments to error logs in redis-service for clarity

--- src/services/redis-service.mjs
+++ src/services/redis-service.mjs
@@ -9,7 +9,7 @@ const setValue = async (key, value, timeExpiration = null) => {
             await redisClient.set(key, stringValue);
         }
     } catch (err) {
-        console.log("No se pudo setear el valor", err)
+        console.log("No se pudo setear el valor", err) //ACAAAAAAAAAAAAAAAAAA
     }
 }
 const getValue = async (key) => {
@@ -17,13 +17,13 @@ const getValue = async (key) => {
         const data = await redisClient.get(key);
         return data ? JSON.parse(data) : null;
     } catch (error) {
-        console.log("No se pudo obtener el valor", err);
+        console.log("No se pudo obtener el valor", err); //ACAAAAAAAAAAAAAAAAAA
     }
 }
 const deleteValue = async (key) => {
     try {
         await redisClient.del(key);
     } catch (error) {
-        console.log("No se pudo borrar el valor", err);
+        console.log("No se pudo borrar el valor", err); //ACAAAAAAAAAAAAAAAAAA
     }
 }
